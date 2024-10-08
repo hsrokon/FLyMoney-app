889dc63c4b72b0453576b3ccbc0031697daad1af
MD. HUSSAIN SHAHRIAR ROKON <hsrokonofficial@gmail.com>
2024-10-09 00:36:36 +0600

    Added on-click function to quickly back n forth between sections on on-page exchange between add money to cash-out and so on.
    learned a lot of the moneyOptions functions inconsistency turns out to no inconsistency but on-click assigned on smaller are like text rather than the entire parent div

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,47 @@
 function logOut() {
     window.location.href='index.html'
 }
+
+const defaultHome = document.getElementById('default-home');
+
+//This is also usable
+// const addMoney = document.getElementById('add-money');
+// const cashOut = document.getElementById('cash-out');
+// const sendMoney = document.getElementById('send-money');
+// const cashBack = document.getElementById('cashback');
+// const payBill = document.getElementById('pay-bill');
+// const transaction = document.getElementById('transaction');
+
+// function moneyOptions(selectedOption) {
+    
+//     if (!defaultHome.classList.contains('hidden')) {
+//         defaultHome.classList.add('hidden');
+//     } 
+
+//     const sections = [addMoney,cashOut,sendMoney,cashBack,payBill,transaction];
+//     sections.forEach(section => {
+//         if (!section.classList.contains("hidden")) {
+//           section.classList.add("hidden")  
+//         }
+//     });
+
+//     document.getElementById(selectedOption).classList.remove("hidden");
+// }
+
+
+function moneyOptions(selectedOption) {
+
+    defaultHome.classList.add('hidden');
+
+    const sections = ["add-money", "cash-out", "send-money", "cashback", "pay-bill", "transaction"];
+    
+    sections.forEach(section => {
+        const sectionElement = document.getElementById(section);
+
+        if (section === selectedOption) {
+            sectionElement.classList.remove('hidden');  
+        } else {
+            sectionElement.classList.add('hidden');    
+        }
+    });
+}
