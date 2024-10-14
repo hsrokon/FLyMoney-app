42d8ebe376b8ef382deae9dcbcfd98185e662dc7
MD. HUSSAIN SHAHRIAR ROKON <hsrokonofficial@gmail.com>
2024-10-14 19:36:07 +0600

    Transaction Pop-Up added

--- scripts/home.js
+++ scripts/home.js
@@ -5,7 +5,6 @@ function logOut() {
 }
 
 const defaultHome = document.getElementById('default-home');
-
 //This is also usable
 // const addMoney = document.getElementById('add-money');
 // const cashOut = document.getElementById('cash-out');
@@ -13,28 +12,21 @@ const defaultHome = document.getElementById('default-home');
 // const cashBack = document.getElementById('cashback');
 // const payBill = document.getElementById('pay-bill');
 // const transaction = document.getElementById('transaction');
-
-// function moneyOptions(selectedOption) {
-    
+// function moneyOptions(selectedOption) {   
 //     if (!defaultHome.classList.contains('hidden')) {
 //         defaultHome.classList.add('hidden');
 //     } 
-
 //     const sections = [addMoney,cashOut,sendMoney,cashBack,payBill,transaction];
 //     sections.forEach(section => {
 //         if (!section.classList.contains("hidden")) {
 //           section.classList.add("hidden")  
 //         }
 //     });
-
 //     document.getElementById(selectedOption).classList.remove("hidden");
 // }
-
-
 function moneyOptions(selectedOption) {
 
     defaultHome.classList.add('hidden');
-
     const sections = ["add-money", "cash-out", "send-money", "cashback", "pay-bill", "transaction"];
     
     sections.forEach(section => {
@@ -47,3 +39,22 @@ function moneyOptions(selectedOption) {
         }
     });
 }
+
+// Transaction PopUp 
+function initialFun() {
+    const amount = 500;
+    popupFun(amount);
+}
+
+const popUp = document.getElementById('transactionModal');
+
+function popupFun(amount) {
+    popUp.classList.remove('hidden');
+
+    const TranAmount = document.getElementById('transactionAmount');
+    TranAmount.innerText=`$${amount}`;
+}
+
+document.getElementById('closeModal').addEventListener('click',()=> {
+    popUp.classList.add('hidden');
+})
